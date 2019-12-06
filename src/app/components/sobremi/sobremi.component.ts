5fb9530ecea19bf200520fb6b498bdd4f5aff6db
KevinJS <iammaindiana@gmail.com>
2019-12-06 12:58:20 -0500

    Sobre terminado

--- src/app/components/sobremi/sobremi.component.ts
+++ src/app/components/sobremi/sobremi.component.ts
@@ -6,8 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sobremi.component.css']
 })
 export class SobremiComponent implements OnInit {
-
-  constructor() { }
+public title:string;
+public subtitle:string;
+public email:string;
+  constructor() {
+    this.title = "MisterJS"
+    this.subtitle = "Desarrollador web y movil "
+    this.email = "iammaindiana@gmail.com"
+   }
 
   ngOnInit() {
   }
